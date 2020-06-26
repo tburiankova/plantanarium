68f052aee58c35239d72df422107aebf739562d9
tereza <tburiankova@gmail.com>
2020-06-26 18:49:32 +0100

    Cart functionality finished

--- components/Cart/CartItem.js
+++ components/Cart/CartItem.js
@@ -1,14 +1,32 @@
-function CartItem() {
+import Link from 'next/link';
+
+function CartItem({ product, handleRemoveFromCart }) {
+  const { name, price, mediaUrl, _id } = product.product;
+  const { quantity } = product;
+
   return (
     <>
       <div className="cart__item">
-        <div className="cart__item__img"></div>
+        <div className="cart__item__img">
+          <img src={mediaUrl} alt={name} />
+        </div>
         <div className="cart__item__info">
-          <h3>Product Name</h3>
-          <p>1x £ 0.00</p>
+          <Link href={`/product?_id=${_id}`}>
+            <a>
+              <h3>{name}</h3>
+            </a>
+          </Link>
+          <p>
+            {quantity}x £ {price.toFixed(2)}
+          </p>
         </div>
         <div className="cart__item__remove">
-          <button className="btn-delete">Remove</button>
+          <button
+            className="btn-delete"
+            onClick={() => handleRemoveFromCart(_id)}
+          >
+            Remove
+          </button>
         </div>
       </div>
     </>
--- pages/cart.js
+++ pages/cart.js
@@ -1,33 +1,77 @@
+import { useState, useEffect } from 'react';
 import { parseCookies } from 'nookies';
 import axios from 'axios';
 import baseUrl from '../utils/baseUrl';
+import { useRouter } from 'next/router';
+import calculateCartTotal from '../utils/calculateCartTotal';
+import cookie from 'js-cookie';
 
 // components
 import CartItem from '../components/Cart/CartItem';
 
 function Cart({ products, user }) {
+  const [cartProducts, setCartProducts] = useState(products);
+  const [isCarEmpty, setCartEmpty] = useState(false);
+  const [cartAmount, setCartAmount] = useState(0);
+  const [stripeAmount, setStripeAmount] = useState(0);
+  const router = useRouter();
+
+  useEffect(() => {
+    const { cartTotal, stripeTotal } = calculateCartTotal(products);
+    setCartAmount(cartTotal);
+    setStripeAmount(stripeTotal);
+    setCartEmpty(products.length === 0);
+  }, [products]);
+
+  async function handleRemoveFromCart(productId) {
+    const url = `${baseUrl}/api/cart`;
+    const token = cookie.get('token');
+    const payload = {
+      params: { productId },
+      headers: { Authorization: token },
+    };
+    const response = await axios.delete(url, payload);
+    setCartProducts(response.data);
+  }
+
   return (
     <>
       <div className="cart__heading">
         <h1>Your cart</h1>
         {user ? (
           <>
-            <h2>No plants in your cart yet. Why don't you add some?</h2>
-            <button className="btn-main">View Products</button>
+            {products.length === 0 ? (
+              <h2>No plants in your cart yet. Why don't you add some?</h2>
+            ) : (
+              <h2>Shop some more!</h2>
+            )}
+            <button className="btn-main" onClick={() => router.push('/store')}>
+              View Products
+            </button>
           </>
         ) : (
           <>
             <h2>Sign in to add plants to your cart!</h2>
-            <button className="btn-main">Sign In</button>
+            <button className="btn-main" onClick={() => router.push('/login')}>
+              Sign In
+            </button>
           </>
         )}
       </div>
       <div className="cart__container">
         <div className="cart__container--inner">
-          <CartItem />
+          {cartProducts.map((product) => (
+            <CartItem
+              key={product.product._id}
+              product={product}
+              handleRemoveFromCart={handleRemoveFromCart}
+            />
+          ))}
           <div className="cart__summary">
-            <p>Total: £ 0.00</p>
-            <button className="btn-submit">Checkout</button>
+            <p>Total: £ {cartAmount}</p>
+            <button className="btn-submit" disabled={isCarEmpty}>
+              Checkout
+            </button>
           </div>
         </div>
       </div>
